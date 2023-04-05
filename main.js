36b9dafe41ee822d8311320a5e23a182dd713c92
najulxz <129889695+najulxz@users.noreply.github.com>
2023-04-05 11:10:09 -0300

    alteracao cor e nome

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 document.addEventListener( "click" , showcolor );
 
+document.getElementById( "botao" ).addEventListener( "click" , exibenome);
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
 }
 
+function exibenome()
+{
+    let usuario = document.getElementById( "nome" ).value ;
+    document.getElementById( "mensagem" ).innerText = " meu nome e: " + usuario
+}
