d0480ba239444ed1a75fe8e9c9aab4e5ae778643
najulxz <129889695+najulxz@users.noreply.github.com>
2023-04-05 13:58:53 -0300

    edição sinais

--- main.js
+++ main.js
@@ -29,19 +29,19 @@ function dividir()
 {
     let num3 = document.getElementById( "num3" ).value;
     let num4 = document.getElementById( "num4" ).value;
-    document.getElementById( "resultado2").innerHTML =  parseFloat( num3 ) + parseFloat( num4 );
+    document.getElementById( "resultado2").innerHTML =  parseFloat( num3 ) / parseFloat( num4 );
 }
 
 function multiplicar()
 {
     let num5 = document.getElementById( "num5" ).value;
     let num6 = document.getElementById( "num6" ).value;
-    document.getElementById( "resultado3").innerHTML =  parseFloat( num5 ) + parseFloat( num6 );
+    document.getElementById( "resultado3").innerHTML =  parseFloat( num5 ) * parseFloat( num6 );
 }
 
 function subtrair()
 {
     let num7 = document.getElementById( "num7" ).value;
     let num8 = document.getElementById( "num8" ).value;
-    document.getElementById( "resultado4").innerHTML =  parseFloat( num7 ) + parseFloat( num8 );
+    document.getElementById( "resultado4").innerHTML =  parseFloat( num7 ) - parseFloat( num8 );
 }
