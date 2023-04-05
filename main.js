37609b37864e7bb5361be8a5d8abcd8a3efc1755
najulxz <129889695+najulxz@users.noreply.github.com>
2023-04-05 12:44:07 -0300

    atualizaçao numeros adiçao

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener( "click" , showcolor );
 
 document.getElementById( "botao" ).addEventListener( "click" , exibenome);
+document.getElementById( "soma" ).addEventListener( "click" , somar);
 
 function showcolor()
 {
@@ -14,11 +15,33 @@ function exibenome()
     document.getElementById( "nome" ).value = "" ;
 }
 
-documen.getElementById( "btn-soma" ).addEventListener( "click" , somar);
+
   
 function somar()
 {
+
     let num1 = document.getElementById( "num1" ).value;
-    document.getElementById( "num2" ).innerHTML = "";
-    document.getElementById( "resultado1").value= "num1"+"num2";
+    let num2 = document.getElementById( "num2" ).value;
+    document.getElementById( "resultado1").innerHTML =  parseFloat( num1 ) + parseFloat( num2 );
+}
+
+function dividir()
+{
+    let num1 = document.getElementById( "num3" ).value;
+    document.getElementById( "num4" ).innerHTML = "";
+    document.getElementById( "resultado1").value= "num1"/"num2";
+}
+
+function multiplicar()
+{
+    let num1 = document.getElementById( "num5" ).value;
+    document.getElementById( "num6" ).innerHTML = "";
+    document.getElementById( "resultado1").value= "num1"*"num2";
+}
+
+function subtrair()
+{
+    let num1 = document.getElementById( "num7" ).value;
+    document.getElementById( "num8" ).innerHTML = "";
+    document.getElementById( "resultado1").value= "num1"-"num2";
 }
